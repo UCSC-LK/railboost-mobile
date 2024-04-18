9ea2937921cbba5431e85ec7928022a21f3b5cc3
DewBM <dewmitha.mahiti@gmail.com>
2024-04-19 01:56:57 +0530

    save logged in user info to the app storage.

--- app/src/main/java/org/ucsc/railboost_mobile/di/AppModule.kt
+++ app/src/main/java/org/ucsc/railboost_mobile/di/AppModule.kt
@@ -3,16 +3,16 @@ package org.ucsc.railboost_mobile.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.components.SingletonComponent
 import org.ucsc.railboost_mobile.repository.LoginRepo
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 object AppModule {
 
     @Provides
-    @Singleton
     fun provideLoginRepo(): LoginRepo {
         return LoginRepo()
     }
--- app/src/main/java/org/ucsc/railboost_mobile/viewmodels/LoginViewModel.kt
+++ app/src/main/java/org/ucsc/railboost_mobile/viewmodels/LoginViewModel.kt
@@ -2,14 +2,18 @@ package org.ucsc.railboost_mobile.viewmodels
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import org.ucsc.railboost_mobile.data.LoginResponseDTO
 import org.ucsc.railboost_mobile.repository.LoginRepo
+import org.ucsc.railboost_mobile.utils.DataStoreManager
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
-    private val loginRepo: LoginRepo
+    private val loginRepo: LoginRepo,
+    private val dataStoreManager: DataStoreManager
 ) : ViewModel() {
 
     fun onLoginButtonClick(username: String, password: String) {
@@ -18,8 +22,16 @@ class LoginViewModel @Inject constructor(
 
 
     fun onLoginSuccess(loginResp: LoginResponseDTO) {
+        saveUserInfo(loginResp)
         Log.d("login", loginResp.username)
         Log.d("login", loginResp.role.toString())
         Log.d("login", loginResp.jwt)
+    }
+
+    private fun saveUserInfo(loginResp: LoginResponseDTO) {
+        viewModelScope.launch {
+            dataStoreManager.setData("username", loginResp.username)
+            dataStoreManager.setData("jwt", loginResp.jwt)
+        }
     }
 }
